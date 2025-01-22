54553b1d2f5bef0fb0b460cec9a56d6ed4bd4032
caroDesign <juego.fuego@gmail.com>
2025-01-22 00:29:10 -0500

    Tailwind setup for project, added Google Fonts and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,22 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['"Manrope"', 'ui-sans-serif', 'system-ui'],
+    },
+    colors: {
+      'fiord': 'hsl(217, 19%, 35%)',
+      'lynch': 'hsl(214, 17%, 51%)',
+      'cadet-blue': 'hsl(212, 23%, 69%)',
+      'alice-blue': 'hsl(210, 46%, 95%)',
+      'white': 'hsl(0, 0%, 100%)',
+    },
+    extend: {
+      screens: {
+        '2xs': '375px',
+        'xs': '425px',
+      }
+    },
   },
   plugins: [],
 }
-
